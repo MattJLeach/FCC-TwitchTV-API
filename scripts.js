ea7cae0d3c598d83460d7f098e8116ddd0078b84
Matt Leach <email@mattleach.info>
2016-10-16 16:07:50 +0100

    First Commit

---
+++ scripts.js
@@ -0,0 +1,32 @@
+// Array of channels
+var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+// Function for making AJAX call
+function getDetails(channel, callback) {
+	$.ajax({
+		url: 'https://wind-bow.hyperdev.space/twitch-api/channels/' + channel,
+		type: 'GET',
+		dataType: 'jsonp',
+		success: function(data) {
+			callback(data);
+		},
+		error: function(err) {
+			alert('Oops, you broke it');
+		}
+	});
+}
+
+$(function() {
+
+	// On load run the AJAX function for each channel in the array
+	for( var i = 0; i < channels.length; i++ ) {
+		getDetails(channels[i], function(data) {
+			// Append channel details to #output
+			$('#output').append('<div class="channelPanel">' + 
+				'<img src="' + data.logo + '">' +
+				'<div class="channelName">' + data.display_name + '</div>' +
+				'<div class="channelStatus">' + data.status + '</div></div>');
+		});
+	}
+
+});
